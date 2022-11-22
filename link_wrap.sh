5c9fb0e88b0fa5752282055b57868065e444926e
Jonathan D.K. Gibbons <jonored@gmail.com>
2022-11-22 14:36:17 +0000

    WIP: Accompanying linker wrapper and nanos.json for fix.

---
+++ link_wrap.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rust-lld "$@"
+
+echo RUST_LLD DONE
+
+while [ $# -gt 0 -a "$1" != "-o" ];
+do
+	shift;
+done
+OUT="$2"
+
+echo OUT IS $OUT
+
+armv6m-unknown-none-eabi-objcopy --dump-section .rel.nvm_data=provenance-reloc-2 --dump-section .rel.data=provenance-reloc-3 $OUT provenance-reloc
+cat provenance-reloc-2 provenance-reloc-3 > provenance-reloc-4
+armv6m-unknown-none-eabi-objcopy --update-section .rel_flash=provenance-reloc-4 $OUT
+
