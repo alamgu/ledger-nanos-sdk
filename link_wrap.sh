23ef9fbee1351ba7746cb83983882c198c615ad5
Jonathan D.K. Gibbons <jonored@gmail.com>
2022-11-24 04:04:08 +0000

    Comments/todos in link_wrap.sh.

--- link_wrap.sh
+++ link_wrap.sh
@@ -17,9 +17,14 @@ OUT="$2"
 
 echo OUT IS $OUT
 
+# the relocations for the constants section are required
 ${OBJCOPY} --dump-section .rel.nvm_data=provenance-reloc-2 $OUT provenance-reloc
+# there might not _be_ a nonempty .data section, so there might be no relocations for it; fail gracefully.
 ${OBJCOPY} --dump-section .rel.data=provenance-reloc-3 $OUT provenance-reloc || true
+# Concatenate the relocation sections; this should still write provenance-reloc-4 even if provenance-reloc-3 doesn't exist.
 cat provenance-reloc-2 provenance-reloc-3 > provenance-reloc-4 || true
+# pad the relocs out to size - we should probably make some way to adjust this size from the source.
 truncate -s 7K provenance-reloc-4
+# and write the relocs to their section in the flash image.
 ${OBJCOPY} --update-section .rel_flash=provenance-reloc-4 $OUT
 
