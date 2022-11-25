b9212952ecd8a49563112267e78e6665fdfcf052
Jonathan D.K. Gibbons <jonored@gmail.com>
2022-11-25 18:13:36 +0000

    More error handling, have rel_flash size setting.

--- link_wrap.sh
+++ link_wrap.sh
@@ -18,13 +18,26 @@ OUT="$2"
 echo OUT IS $OUT
 
 # the relocations for the constants section are required
-${OBJCOPY} --dump-section .rel.nvm_data=provenance-reloc-2 $OUT provenance-reloc
+${OBJCOPY} --dump-section .rel.nvm_data=$OUT-nvm-reloc $OUT /dev/null
 # there might not _be_ a nonempty .data section, so there might be no relocations for it; fail gracefully.
-${OBJCOPY} --dump-section .rel.data=provenance-reloc-3 $OUT provenance-reloc || true
+${OBJCOPY} --dump-section .rel.data=$OUT-data-reloc $OUT /dev/null || true
 # Concatenate the relocation sections; this should still write provenance-reloc-4 even if provenance-reloc-3 doesn't exist.
-cat provenance-reloc-2 provenance-reloc-3 > provenance-reloc-4 || true
+cat $OUT-nvm-reloc $OUT-data-reloc > $OUT-relocs || true
 # pad the relocs out to size - we should probably make some way to adjust this size from the source.
-truncate -s 7K provenance-reloc-4
+
+reloc_allocated_size="$((0x$(${NM} $OUT | grep _reloc_size | cut -d' ' -f1)))"
+reloc_real_size="$(stat --format %s $OUT-relocs)"
+# Check that our relocations _actually_ fit.
+if [ "$reloc_real_size" -gt "$reloc_allocated_size" ]
+then
+	echo "Insufficient size for relocs; increase it in build.rs."
+	echo "Available size: " $reloc_allocated_size " Used size: " $reloc_real_size
+	fail
+else
+	echo "Sufficient size:" $reloc_allocated_size $reloc_real_size
+fi
+
+truncate -s $reloc_allocated_size $OUT-relocs
 # and write the relocs to their section in the flash image.
-${OBJCOPY} --update-section .rel_flash=provenance-reloc-4 $OUT
+${OBJCOPY} --update-section .rel_flash=$OUT-relocs $OUT
 
